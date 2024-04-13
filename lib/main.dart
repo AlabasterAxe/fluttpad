b78b3e043e97286cf2ac13673731d82742ba3870
Matthew Keller <matthew.ed.keller@gmail.com>
2024-04-13 10:10:15 -0400

    boom. still got it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,277 @@
-import 'package:flutter/material.dart';
+import 'dart:async' show StreamSubscription;
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/foundation.dart' show Key, kDebugMode;
+import 'package:flutter/material.dart'
+    show
+        AlertDialog,
+        AppBar,
+        CircularProgressIndicator,
+        IconButton,
+        Icons,
+        ListTile,
+        MaterialApp,
+        MaterialPageRoute,
+        Scaffold,
+        ScaffoldMessenger,
+        SnackBar,
+        Switch,
+        TextButton,
+        Theme,
+        showDialog;
+import 'package:flutter/painting.dart' show EdgeInsets, TextAlign;
+import 'package:flutter/services.dart' show PlatformException;
+import 'package:flutter/widgets.dart'
+    show
+        AsyncSnapshot,
+        BuildContext,
+        Builder,
+        Center,
+        Container,
+        FutureBuilder,
+        Icon,
+        IconData,
+        ListView,
+        Navigator,
+        State,
+        StatefulWidget,
+        Text,
+        Widget,
+        runApp;
+import 'package:flutter_midi_command/flutter_midi_command.dart'
+    show MidiCommand, MidiDevice;
+
+import 'controller.dart' show ControllerPage;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() => MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAppState extends State<MyApp> {
+  StreamSubscription<String>? _setupSubscription;
+  final MidiCommand _midiCommand = MidiCommand();
 
-  // This widget is the root of your application.
+  bool _virtualDeviceActivated = false;
+  bool _iOSNetworkSessionEnabled = false;
+
+  bool _didAskForBluetoothPermissions = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setupSubscription = _midiCommand.onMidiSetupChanged?.listen((data) async {
+      if (kDebugMode) {
+        print("setup changed $data");
+      }
+      setState(() {});
+    });
+
+    _updateNetworkSessionState();
+  }
+
+  @override
+  void dispose() {
+    _setupSubscription?.cancel();
+    super.dispose();
+  }
+
+  _updateNetworkSessionState() async {
+    var nse = await _midiCommand.isNetworkSessionEnabled;
+    if (nse != null) {
+      setState(() {
+        _iOSNetworkSessionEnabled = nse;
+      });
+    }
+  }
+
+  IconData _deviceIconForType(String type) {
+    switch (type) {
+      case "native":
+        return Icons.devices;
+      case "network":
+        return Icons.language;
+      case "BLE":
+        return Icons.bluetooth;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  Future<void> _informUserAboutBluetoothPermissions(
+      BuildContext context) async {
+    if (_didAskForBluetoothPermissions) {
+      return;
+    }
+
+    await showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+              'Please Grant Bluetooth Permissions to discover BLE MIDI Devices.'),
+          content: const Text(
+              'In the next dialog we might ask you for bluetooth permissions.\n'
+              'Please grant permissions to make bluetooth MIDI possible.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok. I got it!'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    _didAskForBluetoothPermissions = true;
+
+    return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterMidiCommand Example'),
+          actions: <Widget>[
+            Switch(
+                value: _iOSNetworkSessionEnabled,
+                onChanged: (newValue) {
+                  _midiCommand.setNetworkSessionEnabled(newValue);
+                  setState(() {
+                    _iOSNetworkSessionEnabled = newValue;
+                  });
+                }),
+            Switch(
+                value: _virtualDeviceActivated,
+                onChanged: (newValue) {
+                  setState(() {
+                    _virtualDeviceActivated = newValue;
+                  });
+                  if (newValue) {
+                    _midiCommand.addVirtualDevice(name: "Flutter MIDI Command");
+                  } else {
+                    _midiCommand.removeVirtualDevice(
+                        name: "Flutter MIDI Command");
+                  }
+                }),
+            Builder(builder: (context) {
+              return IconButton(
+                  onPressed: () async {
+                    // Ask for bluetooth permissions
+                    await _informUserAboutBluetoothPermissions(context);
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+                    // Start bluetooth
+                    if (kDebugMode) {
+                      print("start ble central");
+                    }
+                    await _midiCommand
+                        .startBluetoothCentral()
+                        .catchError((err) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(err),
+                      ));
+                    });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                    if (kDebugMode) {
+                      print("wait for init");
+                    }
+                    await _midiCommand
+                        .waitUntilBluetoothIsInitialized()
+                        .timeout(const Duration(seconds: 5), onTimeout: () {
+                      if (kDebugMode) {
+                        print("Failed to initialize Bluetooth");
+                      }
+                    });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+                    if (kDebugMode) {
+                      print("done");
+                    }
+                    // If not show a message telling users what to do
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.refresh));
+            }),
           ],
         ),
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(24.0),
+          child: const Text(
+            "Tap to connnect/disconnect, long press to control.",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        body: Center(
+          child: FutureBuilder(
+            future: _midiCommand.devices,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData && snapshot.data != null) {
+                var devices = snapshot.data as List<MidiDevice>;
+                return ListView.builder(
+                  itemCount: devices.length,
+                  itemBuilder: (context, index) {
+                    MidiDevice device = devices[index];
+
+                    return ListTile(
+                      title: Text(
+                        device.name,
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      subtitle: Text(
+                          "ins:${device.inputPorts.length} outs:${device.outputPorts.length}, ${device.id}, ${device.type}"),
+                      leading: Icon(device.connected
+                          ? Icons.radio_button_on
+                          : Icons.radio_button_off),
+                      trailing: Icon(_deviceIconForType(device.type)),
+                      onLongPress: () {
+                        _midiCommand.stopScanningForBluetoothDevices();
+                        Navigator.of(context)
+                            .push(MaterialPageRoute<void>(
+                          builder: (_) => ControllerPage(device),
+                        ))
+                            .then((value) {
+                          setState(() {});
+                        });
+                      },
+                      onTap: () {
+                        if (device.connected) {
+                          if (kDebugMode) {
+                            print("disconnect");
+                          }
+                          _midiCommand.disconnectDevice(device);
+                        } else {
+                          if (kDebugMode) {
+                            print("connect");
+                          }
+                          _midiCommand.connectToDevice(device).then((_) {
+                            if (kDebugMode) {
+                              print("device connected async");
+                            }
+                          }).catchError((err) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text(
+                                    "Error: ${(err as PlatformException?)?.message}")));
+                          });
+                        }
+                      },
+                    );
+                  },
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
